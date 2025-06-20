3d6b340e896b1b8cbed88d2f7e25a214f51ffbc2
Trent Jones <rtrentjones@gmail.com>
2025-06-19 18:10:40 -0700

    fix: Update Next config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['@heist-mind/database', '@heist-mind/shared'],
+  transpilePackages: ['@heist-mind/database', '@heist-mind/shared', '@heist-mind/ui'],
 };
 
 export default nextConfig;
